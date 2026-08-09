db8253a9a942b554276dc716048b71027251fcb1
NSTradingUS-CA <northsentineltrading@gmail.com>
2026-08-09 11:39:51 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -60,9 +60,9 @@ st.markdown("""
         box-sizing: border-box;
         text-align: center;
         color: #666;
-        font-size: 0.9rem;
-        padding: 0.75rem 0;
-        margin-top: 6rem;
+        font-size: 0.85rem;
+        padding: 0.35rem 0;
+        margin-top: 4.5rem;
         background-color: #0D0D0D;
     }
 </style>
